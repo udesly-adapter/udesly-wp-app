fed07fb415906a82f9e1f102753ae10f31ef3f56
EclipseSrl <dev@eclipse.srl>
2021-09-02 10:50:19 +0200

    beta9

--- assets/frontend-editor/src/index.js
+++ assets/frontend-editor/src/index.js
@@ -4,20 +4,15 @@ import App from './App';
 import './app.css';
 import {onIframeLoad} from "./utils/on-iframe-load";
 
-const iframe = document.getElementById("frontend-editor-frame");
-const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-if (  iframeDoc.readyState  === 'complete' ) {
-    //iframe.contentWindow.alert("Hello");
-    onIframeLoad(iframe);
-} else {
     let loadedInterval = setInterval(() => {
-        console.log(iframeDoc.readyState);
-        if (iframeDoc.readyState  === 'complete') {
+        const iframe = document.getElementById("frontend-editor-frame");
+        const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+        if (iframeDoc.readyState  === 'complete' && iframeDoc.getElementById('udesly-fe-data')) {
             onIframeLoad(iframe);
             clearInterval(loadedInterval)
         }
     }, 150)
-}
+
 
 document.addEventListener('udesly-fe.init', e => {
     const {globalConfig, pageConfig, iframe} = e.detail;
